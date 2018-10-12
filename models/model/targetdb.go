0028c407b22734fe15cffdf9579698c5bd4114b3
chymingyan <chen_haiyan@yeah.net>
2018-10-12 11:32:58 +0800

    midify catalog

---
+++ models/model/targetdb.go
@@ -0,0 +1,9 @@
+package model
+
+func struct targetdb{
+	targetdb_id string
+	targetdb_ip string
+	targetdb_port string
+	targetdb_servername string
+	
+}
---
+++ models/model/targethost.go
@@ -0,0 +1,9 @@
+package model
+
+fun struct targethost{
+	targethost_ip string
+	targethost_port string
+	targethots_username string
+	targethost_password string
+	targethost_type string
+}
